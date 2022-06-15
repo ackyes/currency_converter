9290b41fbd9bbaa1c127e26d0faabb024215d18f
ackyes <nguyt584@hsd.k12.or.us>
2022-06-15 16:20:13 -0700

    tried making convert work

--- currency_converter/app.py
+++ currency_converter/app.py
@@ -1,17 +1,42 @@
 import dearpygui.dearpygui as dpg
 import model
 
+def convert():
+    """ get exchance rate & multiply by amount"""
+    amount = dpg.get_value(amount_id)
+    from_cur = dpg.get_value(from_cur_id)
+    to_cur = dpg.get_value(to_cur_id)
+    
+    # get only the codes
+    from_cur = from_cur.split("-")[0].strip()
+    to_cur = to_cur.split("-")[0].strip()
+
+    # get the exchange rate
+    status, rate = model.get_exchange_rate(from_cur, to_cur)
+    exchange = amount * rate
+    results = "{} {} is equal to {} {}".format(amount, from_cur, exchange, to_cur)
+    dpg.set_value(output_id, results)
+
 dpg.create_context()
+
+# widget tag ids
+amount_id = dpg.generate_uuid()
+from_cur_id = dpg.generate_uuid()
+to_cur_id = dpg.generate_uuid()
+output_id = dpg.generate_uuid()
+
 dpg.create_viewport(title='Custom Title', width=600, height=300)
 
 with dpg.window(label='Currency Convertor', width=600, height=300):
     dpg.add_text("Welcome to the Currency Convertor.")
-    dpg.add_input_float(label="amount", default_value=1.0, width=100)
-    dpg.add_combo(model.currencies, 
-        default_value=model.default_from, label="From", width=250)
-    dpg.add_combo(model.currencies, 
-        default_value=model.default_from, label="To", width=250)
-    dpg.add_button(label="Convert")
+    dpg.add_input_float(label="amount", default_value=1.0, width=100, 
+        tag=amount_id, min_value = 0.01, min_clamped=True)
+    dpg.add_combo(model.currencies, default_value=model.default_from, 
+        tag=from_cur_id, label="From", width=250)
+    dpg.add_combo(model.currencies, default_value=model.default_from, 
+        tag=to_cur_id, label="To", width=250)
+    dpg.add_button(label="Convert", callback=convert)
+    dpg.add_text("", tag=output_id)
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
--- currency_converter/model.py
+++ currency_converter/model.py
@@ -1,3 +1,4 @@
+from pkgutil import extend_path
 import requests
 
 currency_code_map = {
@@ -22,20 +23,35 @@ def get_currencies():
     return currency_string
 
 def get_currency_from_code(code):
-    return currency_code_map[code]
-
-def get_currency_string(code):
     return code + " - " + get_currency_from_code(code)
 
 # API Call function
 url_host = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json"
 response = requests.get(url_host)
-currency_code_map = response.json()
+sample_json = response.json()
 
-currencies = get_currencies()
+def get_exchange_rate(from_code, to_code):
+    url_host = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
+    endpoint = from_code + "/" + to_code
+    ext = ".json"
+    url = url_host + endpoint + ext
+    response = requests.get(url)
+    currency_code_map = response
+    if response.ok:
+        exchange = response.json()
+        rate = exchange[to_code]
+    else:
+        rate = 0
+    return response.ok, rate
+
+def get_currency_string(code):
+    return code + "-" + get_currency_from_code(code)
+
+currencies = get_currencies(currency_code_map)
 default_from = get_currency_string("usd")
 default_to = get_currency_string("eur")
 
 if __name__ == "__main__":
-    get_currencies()
-    print(get_currency_string())
+    get_currencies(currency_code_map)
+    success, rate = get_exchange_rate("usd", eur)
+    print(rate)
